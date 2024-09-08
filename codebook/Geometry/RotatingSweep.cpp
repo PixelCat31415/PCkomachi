45b13e466a8e4d956d5547fc21f151fc54a4f4ea
PixelCat31415 <jimmychiu31415@gmail.com>
2024-09-09 05:47:01 +0800

    [geometry] rotating sweepline & polar angle comparison

--- codebook/Geometry/RotatingSweep.cpp
+++ codebook/Geometry/RotatingSweep.cpp
@@ -1,22 +1,34 @@
-void rotatingSweepLine(vector<pii> &ps) {
-  int n = SZ(ps), m = 0;
-  vector<int> id(n), pos(n);
-  vector<pii> line(n * (n - 1));
-  for (int i = 0; i < n; ++i)
-    for (int j = 0; j < n; ++j)
-      if (i != j) line[m++] = pii(i, j);
-  sort(ALL(line), [&](pii a, pii b) {
-    return cmp(ps[a.Y] - ps[a.X], ps[b.Y] - ps[b.X]);
-  }); // cmp(): polar angle compare
-  iota(ALL(id), 0);
-  sort(ALL(id), [&](int a, int b) {
-    if (ps[a].Y != ps[b].Y) return ps[a].Y < ps[b].Y;
-    return ps[a] < ps[b];
-  }); // initial order, since (1, 0) is the smallest
-  for (int i = 0; i < n; ++i) pos[id[i]] = i;
-  for (int i = 0; i < m; ++i) {
-    auto l = line[i];
-    // do something
-    tie(pos[l.X], pos[l.Y], id[pos[l.X]], id[pos[l.Y]]) = make_tuple(pos[l.Y], pos[l.X], l.Y, l.X);
+// pts: 1-indexed Pt array
+int ord[MAXN + 10];
+int rk[MAXN + 10];
+void rotSwpline(int n, Pt* pts) {
+  using E = pair<Pt, pii>;
+  vector<E> ev;  // dir, i, j: (i, j)=>(j, i)
+  rep1(i, n) rep1(j, i - 1) {
+    Pt dir = pts[j] - pts[i];
+    upper(dir) ? ev.pb({dir, {i, j}})
+               : ev.pb({Pt(0, 0) - dir, {j, i}});
+  }
+  sort(ev.begin(), ev.end(), [&](E e1, E e2) {
+    int pol = polarOri(e1.F, e2.F);
+    return pol < 0 || (pol == 0 &&
+        pll(e1.F * pts[e1.S.F], e1.F * pts[e1.S.S])
+      < pll(e1.F * pts[e2.S.F], e1.F * pts[e2.S.S]));
+  });
+  iota(ord + 1, ord + n + 1, 1);
+  sort(ord + 1, ord + n + 1, [&](int i, int j) {
+    return cmpYx(pts[i], pts[j]);
+  });
+  rep1(i, n) rk[ord[i]] = i;
+  // ...init with initial rank...
+  int ne = (int)ev.size();
+  rep(ie, ne) {
+    int i, j; tie(i, j) = ev[ie].S;
+    // ...do order update...
+    rk[i]++; rk[j]--;
+    ord[rk[i]] = i;
+    ord[rk[i] - 1] = j;
+    if(polarOri(ev[ie + 1].F, ev[ie].F) != 0
+      || ie == ne - 1) ; // ...do answer update...
   }
 }
