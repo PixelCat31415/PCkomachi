58f7925ac52fdf76c5b78a00c548a1fe5443ecf5
Fysty <osclin22@gmail.com>
2024-09-24 11:52:30 +0800

    Update default code

--- codebook/Basic/Template.cpp
+++ codebook/Basic/Template.cpp
@@ -1,16 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
-typedef pair<double,double> pdd;
 template<typename T> void _do(T x){cerr<<x<<"\n";}
 template<typename T,typename ...U> void _do(T x,U ...y){cerr<<x<<", ";_do(y...);}
 #define dbg(...) cerr<<#__VA_ARGS__<<" = ";_do(__VA_ARGS__);
 #define MottoHayaku ios::sync_with_stdio(false);cin.tie(0);
-//#define int ll
 #define rep(i,n) for(int i=0;i<n;i++)
 #define rep1(i,n) for(int i=1;i<=n;i++)
 #define F first
@@ -18,3 +14,5 @@ template<typename T,typename ...U> void _do(T x,U ...y){cerr<<x<<", ";_do(y...);
 #define pb push_back
 #define uni(c) c.resize(distance(c.begin(),unique(c.begin(),c.end())))
 #define unisort(c) sort(c.begin(),c.end()),uni(c)
+#define ALL(a) a.begin(),a.end()
+#define SZ(a) ((int)a.size())
