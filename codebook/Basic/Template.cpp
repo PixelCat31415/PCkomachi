fc3225f078839e886cb35c5a753c2af376b98908
Fysty <osclin22@gmail.com>
2024-11-14 21:41:31 +0800

    Update estimation and optimize space

--- codebook/Basic/Template.cpp
+++ codebook/Basic/Template.cpp
@@ -1,21 +1,8 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
 template<typename T> void _do(T x){cerr<<x<<"\n";}
 template<typename T,typename ...U> void _do(T x,U ...y){cerr<<x<<", ";_do(y...);}
 #define dbg(...) cerr<<#__VA_ARGS__<<" = ";_do(__VA_ARGS__);
-#define MottoHayaku ios::sync_with_stdio(false);cin.tie(0);
-#define rep(i,n) for(int i=0;i<n;i++)
-#define rep1(i,n) for(int i=1;i<=n;i++)
-#define F first
-#define S second
-#define pb push_back
 #define uni(c) c.resize(distance(c.begin(),unique(c.begin(),c.end())))
 #define unisort(c) sort(c.begin(),c.end()),uni(c)
-#define ALL(a) a.begin(),a.end()
-#define SZ(a) ((int)a.size())
 auto SEED = chrono::steady_clock::now().time_since_epoch().count();
 mt19937 rng(SEED);
 // cpp $1 -dD -P -fpreprocessed | tr -d '[:space:]' | md5sum | cut -c-6
