bd2dfbaee7947533d8f297f368106b9055729d1c
PixelCat31415 <jimmychiu31415@gmail.com>
2025-09-24 21:54:22 +0800

    (convex hull) renamed Pt to PT; simplified things with rep macro

--- codebook/Geometry/ConvexHull.cpp
+++ codebook/Geometry/ConvexHull.cpp
@@ -1,16 +1,17 @@
-vector <Pt> ConvexHull(vector <Pt> pt) {
-  int n = pt.size();
-  sort(all(pt), [&](Pt a, Pt b) {return a.x == b.x ? a.y < b.y : a.x < b.x;});
-  vector <Pt> ans = {pt[0]};
-  for (int t : {0, 1}) {
-    int m = ans.size();
-    for (int i = 1; i < n; ++i) {
-      while (ans.size() > m && ori(ans[ans.size() - 2], ans.back(), pt[i]) <= 0)
+#define sz(x) ((int)x.size())
+vector<PT> ConvexHull(vector<PT> pt) {
+  int n = sz(pt);
+  sort(pt.begin(), pt.end(), [&](PT a, PT b) {return a.x == b.x ? a.y < b.y : a.x < b.x;});
+  vector<PT> ans = {pt[0]};
+  rep(t, 2) {
+    int m = sz(ans);
+    rep1(i, n - 1) {
+      while (sz(ans) > m && ori(ans[sz(ans) - 2], ans.back(), pt[i]) <= 0)
         ans.pop_back();
       ans.pb(pt[i]);
     }
-    reverse(all(pt));
+    reverse(pt.begin(), pt.end());
   }
-  if (ans.size() > 1) ans.pop_back();
+  if (sz(ans) > 1) ans.pop_back();
   return ans;
 }
