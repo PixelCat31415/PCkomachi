8f53267cc7d54c454d66ca0b5f0a1dc8c289afc3
PixelCat31415 <jimmychiu31415@gmail.com>
2025-09-24 20:55:04 +0800

    (basic geometry) adjusted spacing for easier reading

--- codebook/Geometry/Basic.cpp
+++ codebook/Geometry/Basic.cpp
@@ -6,10 +6,12 @@ T abs2(Pt a){return a*a;}
 double abs(Pt a){return sqrt(a*a);}
 int sign(T a){return abs(a)<eps?0:a>0?1:-1;}
 int ori(Pt a,Pt b,Pt c){return sign((b-a)^(c-a));}
-bool collinear(Pt a,Pt b,Pt c){return sign((a-c)^(b-c))==0;}
-bool btw(Pt a,Pt b,Pt c){return !collinear(a,b,c)?0:sign((a-c)*(b-c))<=0;}//is C between AB
-Pt proj(Pt a,Pt b,Pt c){return (b-a)*((c-a)*(b-a)/abs2(b-a));}//ac projection on ab
-double dist(Pt a,Pt b,Pt c){return abs((c-a)^(b-a))/abs(b-a);}//distance from C to AB
-Pt ccw90(Pt p1){return Pt(-p1.y, p1.x);}
+bool btw(Pt a,Pt b,Pt c){  //is C between AB
+  return ori(a,b,c)?0:sign((a-c)*(b-c))<=0;}
+Pt proj(Pt a,Pt b,Pt c){  //ac projection on ab
+  return (b-a)*((c-a)*(b-a)/abs2(b-a));}
+double dist(Pt a,Pt b,Pt c){  //distance from C to AB
+  return abs((c-a)^(b-a))/abs(b-a);}
+Pt ccw90(Pt p){return Pt(-p.y, p.x);}
 struct Line{Pt a,b;};
 struct Cir{Pt o;double r;};
