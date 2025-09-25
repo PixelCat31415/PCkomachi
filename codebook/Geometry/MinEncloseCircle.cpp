123d4fbfc30e8ceb632d1c7738b15f4690ab874f
PixelCat31415 <jimmychiu31415@gmail.com>
2025-09-25 15:14:12 +0800

    replaced lowercase Pt with uppercase PT

--- codebook/Geometry/MinEncloseCircle.cpp
+++ codebook/Geometry/MinEncloseCircle.cpp
@@ -1,14 +1,14 @@
-Pt circenter(Pt a, Pt b, Pt c) {
-  Pt ab = (a + b) / 2, ac = (a + c) / 2;
+PT circenter(PT a, PT b, PT c) {
+  PT ab = (a + b) / 2, ac = (a + c) / 2;
   return intersect(
     ab, ab + ccw90(b - a),
     ac, ac + ccw90(c - a)
   );
 }
-Cir min_enclosing(vector<Pt> &p) {
+Cir min_enclosing(vector<PT> &p) {
   shuffle(p.begin(), p.end(), mt19937(clock()));
   double r = 0;
-  Pt cent = p[0];
+  PT cent = p[0];
   rep(i, p.size()) if (abs2(cent - p[i]) > r) {
     cent = p[i]; r = 0;
     rep(j, i) if (abs2(cent - p[j]) > r) {
--- codebook/Geometry/Tangent.cpp
+++ codebook/Geometry/Tangent.cpp
@@ -1,18 +1,18 @@
-Pt unit(Pt p) { return p / abs(p); }
-Pt ccw(Pt p, double t) {
-  return Pt(
+PT unit(PT p) { return p / abs(p); }
+PT ccw(PT p, double t) {
+  return PT(
     p.x * cos(t) - p.y * sin(t),
     p.x * sin(t) + p.y * cos(t)
   );
 }
-vector<Line> tangent(Cir c, Pt p) {
+vector<Line> tangent(Cir c, PT p) {
   vector<Line> z;
   double d = abs(p - c.o);
   if (sign(d - c.r) == 0) {
     z.push_back({p, p + ccw90(p - c.o)});
   } else if (d > c.r) {
     double o = acos(c.r / d);
-    Pt i = unit(p - c.o), j = ccw(i, o) * c.r, k = ccw(i, -o) * c.r;
+    PT i = unit(p - c.o), j = ccw(i, o) * c.r, k = ccw(i, -o) * c.r;
     z.push_back({c.o + j, p});
     z.push_back({c.o + k, p});
   }
@@ -24,14 +24,14 @@ vector <Line> tangent(Cir c1, Cir c2, int sign1) {
   double d_sq = abs2(c1.o - c2.o);
   if (sign(d_sq) == 0) return ret;
   double d = sqrt(d_sq);
-  Pt v = (c2.o - c1.o) / d;
+  PT v = (c2.o - c1.o) / d;
   double c = (c1.r - sign1 * c2.r) / d;
   if (c * c > 1) return ret;
   double h = sqrt(max(0.0, 1.0 - c * c));
   for (int sign2 = 1; sign2 >= -1; sign2 -= 2) {
-    Pt n = Pt(v.X * c - sign2 * h * v.Y, v.Y * c + sign2 * h * v.X);
-    Pt p1 = c1.o + n * c1.r;
-    Pt p2 = c2.o + n * (c2.r * sign1);
+    PT n = PT(v.X * c - sign2 * h * v.Y, v.Y * c + sign2 * h * v.X);
+    PT p1 = c1.o + n * c1.r;
+    PT p2 = c2.o + n * (c2.r * sign1);
     if (sign(p1.X - p2.X) == 0 && sign(p1.Y - p2.Y) == 0)
       p2 = p1 + ccw90(c2.o - c1.o);
     ret.pb({p1, p2});
