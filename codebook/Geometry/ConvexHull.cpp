cddac825e9548f05f5973c07eacee0fe0f0992f2
PixelCat31415 <jimmychiu31415@gmail.com>
2025-09-25 11:20:01 +0800

    spacing adjustments

--- codebook/Geometry/ConvexHull.cpp
+++ codebook/Geometry/ConvexHull.cpp
@@ -1,13 +1,12 @@
 #define sz(x) ((int)x.size())
 vector<PT> ConvexHull(vector<PT> pt) {
   int n = sz(pt);
-  sort(pt.begin(), pt.end(), [&](PT a, PT b) {return a.x == b.x ? a.y < b.y : a.x < b.x;});
+  sort(pt.begin(), pt.end(), [&](PT a, PT b) { return make_pair(a.x, a.y) < make_pair(b.x, b.y); });
   vector<PT> ans = {pt[0]};
   rep(t, 2) {
     int m = sz(ans);
     rep1(i, n - 1) {
-      while (sz(ans) > m && ori(ans[sz(ans) - 2], ans.back(), pt[i]) <= 0)
-        ans.pop_back();
+      while (sz(ans) > m && ori(ans[sz(ans) - 2], ans.back(), pt[i]) <= 0) ans.pop_back();
       ans.pb(pt[i]);
     }
     reverse(pt.begin(), pt.end());
