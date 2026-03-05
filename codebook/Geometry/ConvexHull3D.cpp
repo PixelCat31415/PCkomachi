4a980c24c982ae60a79af40097ec282cb95c24a9
Fysty <osclin22@gmail.com>
2026-03-05 15:40:36 +0800

    copy 3d convex hull from std_abs

--- codebook/Geometry/ConvexHull3D.cpp
+++ codebook/Geometry/ConvexHull3D.cpp
@@ -1,62 +1,55 @@
-struct convex_hull_3D {
 struct Face {
   int a, b, c;
-  Face(int ta, int tb, int tc): a(ta), b(tb), c(tc) {}
-}; // return the faces with pt indexes
-vector<Face> res;
-vector<Point> P;
-convex_hull_3D(const vector<Point> &_P): res(), P(_P) {
-// all points coplanar case will WA, O(n^2)
-  int n = SZ(P);
-  if (n <= 2) return; // be careful about edge case
-  // ensure first 4 points are not coplanar
-  swap(P[1], *find_if(ALL(P), [&](auto p) { return sign(abs2(P[0] - p)) != 0; }));
-  swap(P[2], *find_if(ALL(P), [&](auto p) { return sign(abs2(cross3(p, P[0], P[1]))) != 0; }));
-  swap(P[3], *find_if(ALL(P), [&](auto p) { return sign(volume(P[0], P[1], P[2], p)) != 0; }));
-  vector<vector<int>> flag(n, vector<int>(n));
-  res.emplace_back(0, 1, 2); res.emplace_back(2, 1, 0);
-  for (int i = 3; i < n; ++i) {
-    vector<Face> next;
-    for (auto f : res) {
-      int d = sign(volume(P[f.a], P[f.b], P[f.c], P[i]));
-      if (d <= 0) next.pb(f);
-      int ff = (d > 0) - (d < 0);
-      flag[f.a][f.b] = flag[f.b][f.c] = flag[f.c][f.a] = ff;
-    }
-    for (auto f : res) {
-      auto F = [&](int x, int y) {
-        if (flag[x][y] > 0 && flag[y][x] <= 0)
-          next.emplace_back(x, y, i);
-      };
-      F(f.a, f.b); F(f.b, f.c); F(f.c, f.a);
-    }
-    res = next;
-  }
-}
-bool same(Face s, Face t) {
-  if (sign(volume(P[s.a], P[s.b], P[s.c], P[t.a])) != 0) return 0;
-  if (sign(volume(P[s.a], P[s.b], P[s.c], P[t.b])) != 0) return 0;
-  if (sign(volume(P[s.a], P[s.b], P[s.c], P[t.c])) != 0) return 0;
-  return 1;
-}
-int polygon_face_num() {
-  int ans = 0;
-  for (int i = 0; i < SZ(res); ++i)
-    ans += none_of(res.begin(), res.begin() + i, [&](Face g) { return same(res[i], g); });
-  return ans;
-}
-double get_volume() {
-  double ans = 0;
-  for (auto f : res)
-    ans += volume(Point(0, 0, 0), P[f.a], P[f.b], P[f.c]);
-  return fabs(ans / 6);
-}
-double get_dis(Point p, Face f) {
-  Point p1 = P[f.a], p2 = P[f.b], p3 = P[f.c];
-  double a = (p2.y - p1.y) * (p3.z - p1.z) - (p2.z - p1.z) * (p3.y - p1.y);
-  double b = (p2.z - p1.z) * (p3.x - p1.x) - (p2.x - p1.x) * (p3.z - p1.z);
-  double c = (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
-  double d = 0 - (a * p1.x + b * p1.y + c * p1.z);
-  return fabs(a * p.x + b * p.y + c * p.z + d) / sqrt(a * a + b * b + c * c);
-}
+  Face(int _a, int _b, int _c) : a(_a), b(_b), c(_c) {}
 };
+auto preprocess(auto pts) {
+  auto G = pts.begin();
+  vector<int> id;
+  auto fail = tuple{-1, -1, -1, id};
+  int a = find_if(all(pts), [&](Pt z) {
+    return z != *G; }) - G;
+  if (a == sz(pts)) return fail;
+  int b = find_if(all(pts), [&](Pt z) {
+    return cross3(*G, pts[a], z) != Pt(0, 0, 0); }) -G;
+  if (b == sz(pts)) return fail;
+  int c = find_if(all(pts), [&](Pt z) {
+    return sign(volume(*G, pts[a], pts[b], z)) != 0; }) - G;
+  if (c == sz(pts)) return fail;
+  for (int i = 0; i < sz(pts); i++)
+    if (i != a && i != b && i != c) id.pb(i);
+  return tuple{a, b, c, id};
+}
+// return the faces with pts indexes
+vector<Face> convex_hull_3D(vector<Pt> pts) {
+  int n = sz(pts);
+  if (n <= 3) return {}; // be careful about edge case
+  vector<Face> now;
+  vector<vector<int>> z(n, vector<int>(n));
+  auto [a, b, c, ord] = preprocess(pts);
+  if (a == -1) return {};
+  now.emplace_back(a, b, c); now.emplace_back(c, b, a);
+  for (auto i : ord) {
+    vector<Face> nxt;
+    for (auto &f : now) {
+      auto v = volume(pts[f.a], pts[f.b], pts[f.c], pts[i]);
+      if (sign(v) <= 0) nxt.pb(f);
+      z[f.a][f.b] = z[f.b][f.c] = z[f.c][f.a] = sign(v);
+    }
+    auto F = [&](int x, int y) {
+      if (z[x][y] > 0 && z[y][x] <= 0)
+        nxt.emplace_back(x, y, i);
+    };
+    for (auto &f : now)
+      F(f.a, f.b), F(f.b, f.c), F(f.c, f.a);
+    now = nxt;
+  }
+  return now;
+}
+// n^2 delaunay: facets with negative z normal of
+// convexhull of (x, y, x^2 + y^2), use a pseudo-point
+// (0, 0, inf) to avoid degenerate case
+// test @ SPOJ CH3D
+// double area = 0, vol = 0; // surface area / volume
+// for (auto [a, b, c]: faces)
+//   area += abs(ver(p[a], p[b], p[c]))/2.0,
+//   vol += volume(P3(0, 0, 0), p[a], p[b], p[c])/6.0;
