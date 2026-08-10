95e9933ebe9904a43308592c158addbc2fca664c
PixelCat31415 <jimmychiu31415@gmail.com>
2026-08-10 16:15:11 +0800

    [geometry] updated basic geometry template (integral & fp types)

--- Geometry/Basic.cpp
+++ Geometry/Basic.cpp
@@ -12,3 +12,19 @@ int ori(PT a, PT b, PT c) { // is C to the left of A->B
   return sign((b-a)^(c-a)); }
 bool btw(PT a, PT b, PT c) { // is C between AB
   return !ori(a, b, c) && sign((a-c)*(b-c)) <= 0; }
+PT ccw90(PT p) { return PT(-p.y, p.x); }
+
+//  ori(PT a, PT b, PT c) { // is C to the left of A->B
+// change PT::x, y to FP type, replace sign()
+// needs PT+-PT, PT*/scalar
+const double EPS = 1e-9;
+int sign(int a) { return (a > EPS) - (a < EPS); }
+double abs2(PT p){ return p * p; }
+double abs(PT p){ return sqrt(p * p); }
+PT proj(PT a, PT b, PT c){ // C projected to AB
+  return a+(b-a)*((c-a)*(b-a)/abs2(b-a));}
+double dist(PT a,PT b,PT c){ // distance from C to AB
+  return abs((c-a)^(b-a))/abs(b-a);}
+
+// struct Line{ PT a, b; };
+// struct Cir{ PT o; double r; };
