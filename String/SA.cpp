8e8e3283201787114994019a85f642c15d35aca6
PixelCat31415 <jimmychiu31415@gmail.com>
2026-08-15 17:25:11 +0800

    [string] updated SA & SAIS

--- String/SA.cpp
+++ String/SA.cpp
@@ -1,42 +1,47 @@
 #define FOR(i, a, b) for(int i = a; i <= b; i++)
 #define ROF(i, a, b) for(int i = a; i >= b; i--)
-int sa[N], tmp[2][N], c[N], rk[N], lcp[N];
-void buildSA(string s) {
-  int *x = tmp[0], *y = tmp[1], m = 256, n = (int)s.size();
-  FOR(i, 0, m - 1) c[i] = 0;
-  FOR(i, 0, n - 1) c[x[i] = s[i]]++;
-  FOR(i, 1, m - 1) c[i] += c[i - 1];
-  ROF(i, n - 1, 0) sa[--c[x[i]]] = i;
-  for (int k = 1; k < n; k <<= 1) {
+template<int N>
+struct SA {
+  int sa[N], tmp[2][N], c[N], rk[N], lcp[N];
+  void buildSA(const string &s) { // SCOPE HASH
+    int *x = tmp[0], *y = tmp[1], n = sz(s), m = n;
     FOR(i, 0, m - 1) c[i] = 0;
-    FOR(i, 0, n - 1) c[x[i]]++;
+    FOR(i, 0, n - 1) c[x[i] = s[i]]++;
     FOR(i, 1, m - 1) c[i] += c[i - 1];
-    int p = 0;
-    FOR(i, n - k, n - 1) y[p++] = i;
-    FOR(i, 0, n - 1) if(sa[i] >= k) y[p++] = sa[i] - k;
-    ROF(i, n - 1, 0) sa[--c[x[y[i]]]] = y[i];
-    y[sa[0]] = p = 0;
-    FOR(i, 1, n - 1) {
-      int a = sa[i], b = sa[i - 1];
-      if (!(x[a] == x[b] && a + k < n && b + k < n && x[a + k] == x[b + k])) p++;
-      y[sa[i]] = p;
+    ROF(i, n - 1, 0) sa[--c[x[i]]] = i;
+    for (int k = 1; k < n; k <<= 1) {
+      FOR(i, 0, m - 1) c[i] = 0;
+      FOR(i, 0, n - 1) c[x[i]]++;
+      FOR(i, 1, m - 1) c[i] += c[i - 1];
+      int p = 0;
+      FOR(i, n - k, n - 1) y[p++] = i;
+      FOR(i, 0, n - 1) if(sa[i] >= k) y[p++] = sa[i] - k;
+      ROF(i, n - 1, 0) sa[--c[x[y[i]]]] = y[i];
+      y[sa[0]] = p = 0;
+      FOR(i, 1, n - 1) {
+        int a = sa[i], b = sa[i - 1];
+        if (!(x[a] == x[b] && a + k < n && b + k < n && x[a + k] == x[b + k])) p++;
+        y[sa[i]] = p;
+      }
+      if (n == p + 1) break;
+      swap(x, y), m = p + 1;
     }
-    if (n == p + 1) break;
-    swap(x, y), m = p + 1;
-  }
-}
-void buildLCP(string s) {
-  // lcp[i] = LCP(sa[i - 1], sa[i])
-  // lcp(i, j) = min(lcp[rk[i] + 1], lcp[rk[i] + 2], ..., lcp[rk[j]])
-  int n = (int)s.size(), val = 0;
-  FOR(i, 0, n - 1) rk[sa[i]] = i;
-  FOR(i, 0, n - 1) {
-    if (!rk[i]) lcp[rk[i]] = 0;
-    else {
-      if (val) val--;
-      int p = sa[rk[i] - 1];
-      while (val + i < n && val + p < n && s[val + i] == s[val + p]) val++;
-      lcp[rk[i]] = val;
+  }  // sa[rk[i]] = i
+  void buildLCP(const string &s) { // SCOPE HASH
+    int n = sz(s), val = 0;
+    FOR(i, 0, n - 1) rk[sa[i]] = i;
+    FOR(i, 0, n - 1) {
+      if (!rk[i]) lcp[rk[i]] = 0;
+      else {
+        if (val) val--;
+        int p = sa[rk[i] - 1];
+        while (val + i < n && val + p < n && s[val + i] == s[val + p]) val++;
+        lcp[rk[i]] = val;
+      }
     }
+  }  // lcp[i] = LCP(sa[i - 1], sa[i])
+  void build(const string &s) {
+    buildSA(s);
+    buildLCP(s);
   }
-}
+};
--- String/SAIS_20.cpp
+++ String/SAIS_20.cpp
@@ -1,8 +1,8 @@
-auto sais(const auto &s) {
-  const int n = SZ(s), z = ranges::max(s) + 1;
+auto sais(const auto &s) { // SCOPE HASH
+  const int n = sz(s), z = ranges::max(s) + 1;
   if (n == 1) return vector{0};
   vector<int> c(z); for (int x : s) ++c[x];
-  partial_sum(ALL(c), begin(c));
+  partial_sum(iter(c), begin(c));
   vector<int> sa(n); auto I = views::iota(0, n);
   vector<bool> t(n, true);
   for (int i = n - 2; i >= 0; --i)
@@ -18,8 +18,8 @@ auto sais(const auto &s) {
   };
   vector<int> lms, q(n); lms.reserve(n);
   for (auto x = c; int i : I | is_lms)
-    q[i] = SZ(lms), lms.pb(sa[--x[s[i]]] = i);
-  induce(); vector<int> ns(SZ(lms));
+    q[i] = sz(lms), lms.push_back(sa[--x[s[i]]] = i);
+  induce(); vector<int> ns(sz(lms));
   for (int j = -1, nz = 0; int i : sa | is_lms) {
     if (j >= 0) {
       int len = min({n - i, n - j, lms[q[i] + 1] - i});
@@ -29,24 +29,22 @@ auto sais(const auto &s) {
     }
     j = i;
   }
-  fill(ALL(sa), 0); auto nsa = sais(ns);
+  fill(iter(sa), 0); auto nsa = sais(ns);
   for (auto x = c; int y : nsa | views::reverse)
     y = lms[y], sa[--x[s[y]]] = y;
   return induce(), sa;
 }
-// sa[i]: sa[i]-th suffix is the i-th lexicographically smallest suffix.
-// hi[i]: LCP of suffix sa[i] and suffix sa[i - 1].
 struct Suffix {
-  int n; vector<int> sa, hi, ra;
-  Suffix(const auto &_s, int _n) : n(_n), hi(n), ra(n) {
+  int n; vector<int> sa, lcp, ra;
+  Suffix(const auto &_s, int _n) : n(_n), lcp(n), ra(n) { // SCOPE HASH
     vector<int> s(n + 1); // s[n] = 0;
-    copy_n(_s, n, begin(s)); // _s shouldn't contain 0
+    copy_n(_s.begin(), n, begin(s)); // _s shouldn't contain 0
     sa = sais(s); sa.erase(sa.begin());
     for (int i = 0; i < n; ++i) ra[sa[i]] = i;
     for (int i = 0, h = 0; i < n; ++i) {
       if (!ra[i]) { h = 0; continue; }
       for (int j = sa[ra[i] - 1]; max(i, j) + h < n && s[i + h] == s[j + h];) ++h;
-      hi[ra[i]] = h ? h-- : 0;
+      lcp[ra[i]] = h ? h-- : 0;
     }
-  }
+  } // lcp[i] = LCP(sa[i - 1], sa[i])
 };
