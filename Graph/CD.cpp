be6806a43d351baf03afe80545afd92a0e55f7f8
PixelCat31415 <jimmychiu31415@gmail.com>
2026-08-18 22:28:11 +0800

    [graph] rewrote centroid decomposition

--- Graph/CD.cpp
+++ Graph/CD.cpp
@@ -1,46 +1,24 @@
-vector <int> adj[N];
-struct CentroidDecomposition {
-  // 0-index
-  vector <int> sz, cd_pa;
-  int n;
-  CentroidDecomposition () = default;
-  CentroidDecomposition (int _n) : n(_n) {
-    sz.assign(n, 0), cd_pa.assign(n, -2);
-    dfs_cd(0, -1);
-  }
-  void dfs_sz(int v, int pa) {
-    sz[v] = 1;
-    for (int u : adj[v]) if (u != pa && cd_pa[u] == -2)
-      dfs_sz(u, v), sz[v] += sz[u];
-  }
-  int dfs_cen(int v, int pa, int s) {
-    for (int u : adj[v]) if (u != pa && cd_pa[u] == -2) {
-      if (sz[u] * 2 > s)
-        return dfs_cen(u, v, s);
-    }
-    return v;
-  }
-  vector <int> block;
-  void dfs_cd(int v, int pa) {
-    dfs_sz(v, pa);
-    int c = dfs_cen(v, pa, sz[v]);
-    cd_pa[c] = pa;
-    // centroid D&C
-    for (int u : adj[c]) if (cd_pa[u] == -2) {
-      dfs_ans(u, c);
-      // do something
-    }
-    for (int u : adj[c]) if (cd_pa[u] == -2) {
-      dfs_cd(u, c);
-    }
-  }
-  void dfs_ans(int v, int pa) {
-    // calculate path through centroid
-    // do something
-    // remember delete path from the same size
-    for (int u : adj[v]) if (u != pa && cd_pa[u] == -2)
-      dfs_ans(u, v);
-  }
-  // Centroid Tree Property:
-  // let k = lca(u, v) in Centroid Tree, then dis(u, v) = dis(u, k) + dis(k, v)
-};
+struct CenDecomp {
+  int n, mxdep;
+  vector<int> par, dep;
+  CenDecomp(int _n, const auto &adj): n(_n), mxdep(0), par(n, -1), dep(n, -1) {
+    vector<pii> szs(n); int ns = 0;
+    auto getsz = [&](auto &self, int v, int p) -> int {
+      int sz = 1;
+      for (auto i : adj[v]) if (i != p && dep[i] == -1)
+        sz += self(self, i, v);
+      szs[ns++] = {sz, v};
+      return sz;
+    };
+    auto dfs = [&](auto &self, int v, int p, int d) -> void {
+      int tot = getsz(getsz, v, v), j = 0;
+      while (szs[j].F <= tot / 2) j++;
+      v = szs[j].S; ns = 0;
+      par[v] = p; dep[v] = d;
+      mxdep = max(mxdep, d + 1);
+      for (auto i : adj[v]) if (dep[i] == -1)
+        self(self, i, v, d + 1);
+    };
+    dfs(dfs, 0, -1, 0);
+  }  // adj could be vector<vector> or vector[]
+};  // 0-indexed nodes, par[root] = -1, dep[root] = 0
